173ab7141d63a3114fb035ca89f528e62aa3f2e1
Radicalautistt <56npyfygybys@gmail.com>
2022-06-26 04:20:47 +0300

    add cycle counter

--- src/rei_debug.h
+++ src/rei_debug.h
@@ -2,6 +2,7 @@
 #define REI_DEBUG_H
 
 #include <stdlib.h>
+#include <immintrin.h>
 
 #include "rei_logger.h"
 
@@ -41,6 +42,21 @@
      }                                                                                               \
    } while (0)
 #endif
+
+#define REI_COUNT_CYCLES(proc) do {       \
+  u64 s = __rdtsc ();                     \
+  (void) proc;                            \
+  u64 e = __rdtsc () - s;                 \
+                                          \
+  REI_LOG_INFO (                          \
+    REI_ANSI_YELLOW "%s"                  \
+    REI_ANSI_GREEN " took "               \
+    REI_ANSI_YELLOW "%lu"                 \
+    REI_ANSI_GREEN " cycles to compute.", \
+    #proc,                                \
+    e                                     \
+  );                                      \
+} while (0)
 
 // Stringify rei_result_e for debugging purposes.
 const char* rei_show_result (rei_result_e result);
