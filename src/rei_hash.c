181ed8aae354d1df4cfcd857b80b28999ea1ce7f
Radicalautistt <56npyfygybys@gmail.com>
2022-06-26 03:52:46 +0300

    add (stole from wikipedia) implementation of murmur hash

---
+++ src/rei_hash.c
@@ -0,0 +1,49 @@
+#include "rei_hash.h"
+
+// Basically a stolen sample implementation from wikipedia (I have no idea what it's doing).
+u32 rei_murmur_hash (const u8* key, u64 length, u32 seed) {
+  const u32 c1 = 0xcc9e2d51;
+  const u32 c2 = 0x1b873593;
+  const u32 r1 = 15;
+  const u32 r2 = 13;
+  const u32 m = 5;
+  const u32 n = 0xe6546b64;
+
+  u32 hash = seed;
+
+  for (u64 i = length >> 2; i; --i) {
+    u32 k = *((u32*) key);
+    key += sizeof (u32);
+
+    k *= c1;
+    // what the heck.
+    k = (k << r1) | (k >> 17);
+    k *= c2;
+
+    hash ^= k;
+    hash = (hash << r2) | (hash >> 19);
+    hash = (hash * m) + n;
+  }
+
+  u32 k = 0;
+  for (u64 i = length & 3; i; --i) {
+    k <<= 8;
+    k |= key[i - 1];
+  }
+
+  k *= c1;
+  // what the heck.
+  k = (k << r1) | (k >> 17);
+  k *= c2;
+
+  hash ^= k;
+  hash ^= length;
+
+  hash ^= hash >> 16;
+  hash *= 0x85ebca6b;
+  hash ^= hash >> 13;
+  hash *= 0xc2b2ae35;
+  hash ^= hash >> 16;
+
+  return hash;
+}
---
+++ src/rei_hash.h
@@ -0,0 +1,9 @@
+#ifndef REI_HASH_H
+#define REI_HASH_H
+
+#include "rei_types.h"
+
+// Murmur3 hash.
+u32 rei_murmur_hash (const u8* key, u64 length, u32 seed);
+
+#endif /* REI_HASH_H */
